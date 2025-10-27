df1f268d70768b98951d1bf5e16080b0608fc8b9
Anjum Begum <anjum.appo78@gmail.com>
2025-10-27 13:15:11 +0530

    vision

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 document.getElementById("admissionForm").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Thank you for applying. Please prepare for your admission test. You will receive further information via email.");
